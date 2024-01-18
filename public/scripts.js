08694637b7111df598d739a4ec0e38f49eb6770e
unknown <lmechegaray@wecom.global>
2024-01-18 17:43:17 -0300

    changes

--- public/scripts.js
+++ public/scripts.js
@@ -29,37 +29,43 @@ document.getElementById('miFormulario').addEventListener('submit', function (eve
 
       // Si hay datos, oculta el loader y procesa la información
       if (jsonData) {
-          loaderFather.style.display = 'none';
+        loaderFather.style.display = 'none';
 
-          const contenedor = document.getElementById('contenedor');
-          const contenedor2 = document.getElementById('contenedor2');
-          const contenedor3 = document.getElementById('contenedor3');
+        const contenedor = document.getElementById('contenedor');
+        const contenedor2 = document.getElementById('contenedor2');
+        const contenedor3 = document.getElementById('contenedor3');
+        const contenedor4 = document.getElementById('contenedor4');
 
 
-          jsonData[0].forEach(ele => {
-            // Crea un div por cada objeto
-            const nuevoDiv = document.createElement('div');
-    
-            // Agrega la información del objeto al div
-            nuevoDiv.innerHTML = `<p>Representante legal: </p><p>Nombre: ${ele.nombre}</p><p>RUT: ${ele.rut}</p><p>Fecha: ${ele.fecha}</p>`;
-    
-            // Agrega el nuevo div al contenedor
-            contenedor.appendChild(nuevoDiv);
+        jsonData[0].forEach(ele => {
+          // Crea un div por cada objeto
+          const nuevoDiv = document.createElement('div');
+
+          // Agrega la información del objeto al div
+          nuevoDiv.innerHTML = `<p>Representante legal: </p><p>Nombre: ${ele.nombre}</p><p>RUT: ${ele.rut}</p><p>Fecha: ${ele.fecha}</p>`;
+          contenedor.appendChild(nuevoDiv);
         });
-        const domicilio  = jsonData[1].adress;
+        const domicilio = jsonData[1].adress;
         const nuevoDiv1 = document.createElement('div');
         nuevoDiv1.innerHTML = `<p>Domicilio: ${domicilio}</p>`;
         contenedor2.appendChild(nuevoDiv1);
 
-        const actividad  = jsonData[2]["Glosa descriptiva de actividades económicas"];
+        const actividad = jsonData[2]["Glosa descriptiva de actividades económicas"];
         const nuevoDiv2 = document.createElement('div');
         nuevoDiv2.innerHTML = `<p>Actividades Econòmicas: ${actividad}</p>`;
         contenedor3.appendChild(nuevoDiv2);
+        const periodos= document.createElement('div');
+        periodos.innerHTML='<p>Periodos Tributarios Mensuales</p>';
+        contenedor4.appendChild(periodos)
+        const imagen = document.createElement("img");
 
+        const imagenBase64 = jsonData[3].screenshot
+        imagen.src = "data:image/png;base64," + imagenBase64;;
+        contenedor4.appendChild(imagen);
 
       } else {
-          // Si no hay datos, muestra el loader
-          loaderFather.style.display = 'block';
+        // Si no hay datos, muestra el loader
+        loaderFather.style.display = 'block';
       }
     })
     .catch(error => {
@@ -90,7 +96,7 @@ document.getElementById('miFormulario').addEventListener('submit', function (eve
 //                 })
 //                     .then(response => response.text())
 //                     .then(data => {
-                      
+
 
 //                     })
 //                     .catch(error => {
@@ -98,36 +104,36 @@ document.getElementById('miFormulario').addEventListener('submit', function (eve
 //                     });
 //             });
 
-            /* -------------------------*/
-            const responseContainer = document.getElementById('responseContainer');
+/* -------------------------*/
+const responseContainer = document.getElementById('responseContainer');
 
 document.getElementById('fileUploadForm').addEventListener('submit',
-function (event) {
+  function (event) {
     event.preventDefault();
 
-  const fileInput = document.getElementById('fileInput');
-  const file = fileInput.files[0];
-  console.log(file.name)
-  if (file.name) {
-    const formData = new FormData();
-     formData.append('file', file);
-    fetch('/upload', {
-      method: 'POST',
-      body: formData
-    })
-      .then(response => response)
-      .then(data => {
-        console.log(data)
-        // Crea un div con el contenido del Excel
-        // const excelDiv = document.createElement('div');
-        // excelDiv.textContent = JSON.stringify(data);
-
-        // // Añade el div al contenedor
-        // responseContainer.innerHTML = '';
-        // responseContainer.appendChild(excelDiv);
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+    console.log(file.name)
+    if (file.name) {
+      const formData = new FormData();
+      formData.append('file', file);
+      fetch('/upload', {
+        method: 'POST',
+        body: formData
       })
-      .catch(error => {
-        console.error('Error en la solicitud:', error);
-      });
-  }
-})
+        .then(response => response)
+        .then(data => {
+          console.log(data)
+          // Crea un div con el contenido del Excel
+          // const excelDiv = document.createElement('div');
+          // excelDiv.textContent = JSON.stringify(data);
+
+          // // Añade el div al contenedor
+          // responseContainer.innerHTML = '';
+          // responseContainer.appendChild(excelDiv);
+        })
+        .catch(error => {
+          console.error('Error en la solicitud:', error);
+        });
+    }
+  })
--- router/sendUsers.js
+++ router/sendUsers.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const router = express.Router()
+const { writeFileSync } = require("fs");  // Agrega esta línea para importar writeFileSync
+
 const runAllUsers = require("../modules/main.js")
 
 router.post("/",async (req, res) => {
     const { rut, password } = req.body;
     // console.log("body",req.body);
    const data =     await runAllUsers(rut,password)
-   console.log("caa",data)
+  //  const imagen= data[3].screenshot;
+
+  //  const filePath = 'screenshot.png';
+  //  writeFileSync(filePath, imagen, 'base64');
+
     res.status(200).json(data);
   });
 
